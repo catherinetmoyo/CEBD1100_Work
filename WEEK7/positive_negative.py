0498bc8e20b4da3993720074eb985285bd61f63b
c_moyo <catherinetmoyo@gmail.com>
2020-02-26 20:29:52 -0500

    new function

---
+++ WEEK7/positive_negative.py
@@ -0,0 +1,14 @@
+def isnumbernegative(n):
+    if n < 0:
+        return True
+    return False
+    # or return n < 0
+
+
+my_value = 1555
+
+# print number is negative or number is positive
+if isnumbernegative(my_value):
+    print("number is negative")
+else:
+    print("number is positive")

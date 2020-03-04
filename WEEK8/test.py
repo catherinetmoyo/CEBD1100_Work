4ce2ef1534a5a55cbc558525f92e8dfbf8d97613
c_moyo <catherinetmoyo@gmail.com>
2020-03-04 18:56:58 -0500

    function that returns none

--- WEEK8/test.py
+++ WEEK8/test.py
@@ -1,1 +1,12 @@
-0.1 + 0.2 == 0.3
+def getvalues(n):
+    return None
+
+
+# call a function
+student_number = 1214
+
+
+if getvalues(student_number) == None:
+    print("Student not found")
+else:
+    print("Student found")

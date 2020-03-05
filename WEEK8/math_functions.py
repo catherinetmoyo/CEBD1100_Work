3fb2161d7a8349afe4738906f84eb7b5e0566b50
c_moyo <catherinetmoyo@gmail.com>
2020-03-04 19:05:13 -0500

    create a package

---
+++ WEEK8/math_functions.py
@@ -0,0 +1,7 @@
+def double_integer(n: int):
+    return n+2
+
+
+def is_even(n: int):
+    return n % 2 == 0
+
---
+++ WEEK8/program.py
@@ -0,0 +1,5 @@
+import math_functions as mf
+
+print(mf.double_integer(5))
+print(mf.is_even(155))
+

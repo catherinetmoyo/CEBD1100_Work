f3ba98285ac7094553756f9e6bb2774947cf6e80
c_moyo <catherinetmoyo@gmail.com>
2020-03-04 19:48:35 -0500

    assert

--- WEEK8/deposit.py
+++ WEEK8/deposit.py
@@ -2,7 +2,44 @@ def deposit(amount: float):
 
     assert(amount > 0), "Sorry, the amount deposited must be positive"
 
-value = input("Enter a deposit >")
+
+value = input("Enter an amount of  deposit >")
+
+try:
+    deposit(float(value))
+except AssertionError as error_text:
+    print(error_text)
+
+except Exception as error_text:
+    print("An unhandled error has occurred :" + error_text)
+
+else:
+    print("if no error has occurred, run those lines")
+    # send your email of confirmation of deposit here
 
 
-deposit(float(value))
+
+
+
+
+
+
+
+
+
+
+    ############
+    #OR
+    ############
+
+# def deposit(amount: float):
+#
+#     assert(amount > 0), "Sorry, the amount deposited must be positive"
+#
+#
+# value = input("Enter a deposit >")
+#
+# try:
+#     deposit(float(value))
+# except:
+#     print("Sorry, an error occurred")

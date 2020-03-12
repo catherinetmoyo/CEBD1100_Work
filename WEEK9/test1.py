ae662112f4aeb00afe64beb8fabf7db84ed73683
c_moyo <catherinetmoyo@gmail.com>
2020-03-11 20:38:13 -0400

    pandas

--- WEEK9/test1.py
+++ WEEK9/test1.py
@@ -1,15 +1,17 @@
 import pandas as pd
+import matplotlib.pyplot as mpl
+
 
 # making values non scientific
 pd.options.display.float_format = '{:.2f}'.format
 # display fpr wide monitors
-pd.set_option('display.max_rows', 500)
-pd.set_option('display.max_columns', 20)
-pd.set_option('display.width', 1000)
+# pd.set_option('display.max_rows', 500)
+# pd.set_option('display.max_columns', 20)
+# pd.set_option('display.width', 1000)
 
 # parse_dates = if you see a date make it a date not a string
 # infer_datetime_format = JJ-MM-AA
-df = pd.read_csv("50000 Sales Records.csv", parse_dates=True, infer_datetime_format=True)
+df = pd.read_csv("50000 Sales Records.csv", nrows=50, parse_dates=True, infer_datetime_format=True)
 print(df)
 
 # print the list of the keys aka columns on the csv file
@@ -37,12 +39,35 @@ print(df.total_revenue.describe())
 # print("##########")
 # print(df.describe(include=object))
 
-print("########")
+# print("########")
+# print(df.groupby('region')['units_sold'].sum())
+#
+# # the sum for Europe
+# print(df.groupby('region')['units_sold'].sum()['Europe'])
+
+
 print(df.groupby('region')['units_sold'].sum())
 
-# the sum for Europe
-print(df.groupby('region')['units_sold'].sum()['Europe'])
+a = df.groupby('region').sum()['units_sold']
 
+
+# # histogram
+# a.plot(kind='bar', x='region', y='units_sold')
+# mpl.show()
+#
+# # pie chart
+# a.plot(kind='pie', x='region', y='units_sold')
+# mpl.show()
+
+squares = [1, 4, 9, 16, 25, 500]
+
+mpl.plot(squares, linewidth=15)
+mpl.title("Square Numbers", fontsize=24)
+mpl.xlabel("Value", fontsize=14)
+mpl.ylabel("Square of Value", fontsize=14)
+mpl.tick_params(axis='both', labelsize=14)
+mpl.plot(squares)
+mpl.show()
 
 
 
